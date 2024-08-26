499f0963d3424e14264c7a4ebeb42a6571f13469
Songlin Xu <48842598+songlinxu@users.noreply.github.com>
2024-08-26 13:07:30 -0700

    Create Color.py

---
+++ Color.py
@@ -0,0 +1,5 @@
+
+'''
+#1f77b4 - blue, #ff7f0e - orange, #2ca02c - green, #d62728 - red, #9467bd - purple
+#e377c2 - pink, #7f7f7f - gray, #bcbd22 - yellow-green, #17becf - cyan, #8c564b - brown
+'''
